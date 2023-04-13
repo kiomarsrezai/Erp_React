b759701cfbc893fd9357096ccb62a6fac0a68057
alirezakasirzare <alirezakasir@yahoo.com>
2023-04-13 14:30:01 +0430

    add bullet chart feature

--- src/pages/report/chart/revenue-page.tsx
+++ src/pages/report/chart/revenue-page.tsx
@@ -27,8 +27,7 @@ const data = [
     BudgetProcessId: 1,
     AreaName: 1,
     Mosavab: 4352835000000,
-
-    MosavabDaily: 4352835000000,
+    MosavabDaily: 4352835000000 / 2,
     Expense: 2352835000000,
     NotGet: 4352835000000,
   },
@@ -40,7 +39,7 @@ const data = [
     MosavabLabel: "مصوب",
     ExpenseLabel: "عملکرد",
     Mosavab: 8356672000000,
-    MosavabDaily: 8356672000000,
+    MosavabDaily: 8356672000000 / 1,
     Expense: 6356672000000,
     NotGet: 8356672000000,
   },
@@ -52,7 +51,7 @@ const data = [
     BudgetProcessId: 1,
     AreaName: 3,
     Mosavab: 4244004000000,
-    MosavabDaily: 4244004000000,
+    MosavabDaily: 4244004000000 / 3,
     Expense: 0,
     NotGet: 4244004000000,
   },
@@ -64,7 +63,7 @@ const data = [
     ExpenseLabel: "عملکرد",
     AreaName: 4,
     Mosavab: 2994950000000,
-    MosavabDaily: 2994950000000,
+    MosavabDaily: 2994950000000 / 1.2,
     Expense: 0,
     NotGet: 2994950000000,
   },
@@ -76,7 +75,7 @@ const data = [
     MosavabLabel: "مصوب",
     ExpenseLabel: "عملکرد",
     Mosavab: 2035920000000,
-    MosavabDaily: 2035920000000,
+    MosavabDaily: 2035920000000 / 1.1,
     Expense: 0,
     NotGet: 2035920000000,
   },
@@ -88,7 +87,7 @@ const data = [
     ExpenseLabel: "عملکرد",
     AreaName: 6,
     Mosavab: 1407649000000,
-    MosavabDaily: 1407649000000,
+    MosavabDaily: 1407649000000 / 2,
     Expense: 0,
     NotGet: 1407649000000,
   },
@@ -100,7 +99,7 @@ const data = [
     Mosavab: 1970200000000,
     MosavabLabel: "مصوب",
     ExpenseLabel: "عملکرد",
-    MosavabDaily: 1970200000000,
+    MosavabDaily: 1970200000000 / 4,
     Expense: 0,
     NotGet: 1970200000000,
   },
@@ -112,7 +111,7 @@ const data = [
     ExpenseLabel: "عملکرد",
     AreaName: 8,
     Mosavab: 2334984000000,
-    MosavabDaily: 2334984000000,
+    MosavabDaily: 2334984000000 / 3,
     Expense: 2334984000000,
     NotGet: 2334984000000,
   },
@@ -124,11 +123,43 @@ const data = [
     ExpenseLabel: "عملکرد",
     AreaName: "مرکز",
     Mosavab: 31765033637000,
-    MosavabDaily: 31765033637000,
+    MosavabDaily: 31765033637000 / 2,
     Expense: 0,
     NotGet: 31765033637000,
   },
 ];
+
+const CustomBarWithTarget = (props: any) => {
+  const { fill, x, y, width, height, MosavabDaily, Mosavab } = props;
+
+  const check = MosavabDaily / Mosavab;
+  const customHeight = height * check;
+  const customX = x + width / 4;
+  const customWidth = width / 2;
+  const customY = y + height - customHeight;
+
+  return (
+    <svg>
+      <rect
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        stroke="none"
+        fill="#999"
+      />
+
+      <rect
+        x={customX}
+        y={customY}
+        width={customWidth}
+        height={customHeight}
+        stroke="none"
+        fill={fill}
+      />
+    </svg>
+  );
+};
 
 function ReportRevenueChartPage() {
   return (
@@ -143,7 +174,7 @@ function ReportRevenueChartPage() {
       >
         <ResponsiveContainer width="100%" height="100%">
           <ComposedChart data={data}>
-            <CartesianGrid stroke={grey[200]} />
+            <CartesianGrid stroke={grey[300]} strokeDasharray="3 3" />
             <XAxis dataKey="AreaName" />
             <YAxis />
             <Tooltip
@@ -157,7 +188,14 @@ function ReportRevenueChartPage() {
                 value === "Mosavab" ? MosavabLabel : ExpenseLabel
               }
             />
-            <Bar dataKey="Mosavab" fill={grey[400]} />
+            <Bar
+              dataKey="Mosavab"
+              stackId="a"
+              width={100}
+              shape={<CustomBarWithTarget />}
+              fill={grey[400]}
+            />
+
             <Line type="monotone" dataKey="Expense" stroke={blue[800]} />
           </ComposedChart>
         </ResponsiveContainer>
