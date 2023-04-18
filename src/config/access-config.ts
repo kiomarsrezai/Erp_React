d7ec2430758a2953df2d34d82d592b71484b0ea8
alirezakasirzare <alirezakasir@yahoo.com>
2023-04-18 14:35:17 +0330

    add data to config access

--- src/config/access-config.ts
+++ src/config/access-config.ts
@@ -1,72 +1,142 @@
 import { AccessItemShape } from "types/access-type";
 
+const organField: AccessItemShape = {
+  label: "سازمان",
+  name: "organ",
+  value: [
+    {
+      label: "شهرداری",
+      name: 3,
+    },
+    {
+      label: "سازمانها",
+      name: 4,
+    },
+  ],
+};
+
+const yearField: AccessItemShape = {
+  label: "سال",
+  name: "year",
+  value: [
+    {
+      label: "1401",
+      name: 32,
+    },
+    {
+      label: "1402",
+      name: 33,
+    },
+  ],
+};
+
+const centerField: AccessItemShape = {
+  label: "مرکز",
+  name: "center",
+  value: [
+    {
+      label: "با مرکز",
+      name: 1,
+    },
+    {
+      label: "بدون مرکز",
+      name: 2,
+    },
+  ],
+};
+
+const budgetMethodField: AccessItemShape = {
+  label: "نوع بودجه",
+  name: "bodgetMethod",
+  value: [
+    {
+      label: "درآمد/منابع",
+      name: 1,
+    },
+    {
+      label: "جاری / هزینه ای",
+      name: 2,
+    },
+    {
+      label: "عمرانی / سرمایه ای",
+      name: 3,
+    },
+    {
+      label: "مالی",
+      name: 4,
+    },
+    {
+      label: "دیون قطعی سنواتی",
+      name: 5,
+    },
+  ],
+};
+
+const areaField: AccessItemShape = {
+  label: "منطقه",
+  name: "area",
+  value: [
+    {
+      label: "درآمد/منابع",
+      name: 1,
+    },
+    {
+      label: "جاری / هزینه ای",
+      name: 2,
+    },
+    {
+      label: "عمرانی / سرمایه ای",
+      name: 3,
+    },
+    {
+      label: "مالی",
+      name: 4,
+    },
+    {
+      label: "دیون قطعی سنواتی",
+      name: 5,
+    },
+  ],
+};
+
 export const ACCESS_CONFIG: AccessItemShape[] = [
   {
-    label: "واسط سازمان ها",
-    name: "transfer",
+    label: "گزارش",
+    name: "reportChartRevenue",
     value: [
-      {
-        label: "سال",
-        name: "year",
-        value: [
-          {
-            label: "1400",
-            name: 32,
-          },
-          {
-            label: "1401",
-            name: 33,
-          },
-        ],
-      },
-      {
-        label: "year",
-        name: "year2",
-        value: [
-          {
-            label: "1400",
-            name: 32,
-          },
-          {
-            label: "1401",
-            name: 33,
-          },
-        ],
-      },
+      yearField,
+      organField,
+      centerField,
+      budgetMethodField,
+      { label: "درآمد", name: "revenue" },
+      { label: "فروش اموال", name: "sale" },
+      { label: "وام و اوراق", name: "loan" },
+      { label: "نیابتی", name: "niabati" },
     ],
   },
   {
+    label: "بودجه",
+    name: "reportChartRevenue",
+    value: [yearField, areaField, budgetMethodField],
+  },
+  {
+    label: "بودجه",
+    name: "reportChartRevenue",
+    value: [yearField, areaField, budgetMethodField],
+  },
+  {
+    label: "بودجه تفکیکی",
+    name: "budgetSeprator",
+    value: [yearField, areaField, budgetMethodField],
+  },
+  {
+    label: "متولی ها",
+    name: "reportProctorAbstruct",
+    value: [yearField, areaField],
+  },
+  {
     label: "واسط سازمان ها",
-    name: "transfer2",
-    value: [
-      {
-        label: "year",
-        name: "year2",
-        value: [
-          {
-            label: "1400",
-            name: 32,
-          },
-          {
-            label: "1401",
-            name: 33,
-          },
-        ],
-      },
-      {
-        label: "year",
-        name: "year3",
-        value: [
-          {
-            label: "1400",
-            name: 32,
-          },
-          {
-            label: "1401",
-            name: 33,
-          },
-        ],
-      },
-    ],
+    name: "transfer",
+    value: [yearField, areaField, budgetMethodField],
   },
 ];
--- src/pages/access-page.tsx
+++ src/pages/access-page.tsx
@@ -2,11 +2,14 @@ import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import AdminLayout from "components/layout/admin-layout";
-import CheckboxLabeled from "components/ui/inputs/checkbox-labeled";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
 
+import { ChangeEvent } from "react";
 import { grey } from "@mui/material/colors";
 import { ACCESS_CONFIG } from "config/access-config";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const formatConfigs = () => {
   let formated: any = {};
@@ -37,14 +40,37 @@ function AccessPage() {
     console.log(isOnedItems.join("/"));
   };
 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.checked;
+
+    if (value) {
+      let shouldTurnedItems: any = {};
+
+      const splited: string[] = name.split(".");
+      for (let i = 0; i < splited.length; i++) {
+        const key = splited.slice(0, i + 1).join(".");
+        shouldTurnedItems[key] = true;
+      }
+
+      setFormData((prevSatte: any) => ({ ...prevSatte, ...shouldTurnedItems }));
+    } else {
+      setFormData((prevSatte: any) => ({ ...prevSatte, [name]: value }));
+    }
+  };
+
   const renderItem = (item: any, name: string) => (
     <Stack spacing={1} direction="column" key={name}>
       <Box>
-        <CheckboxLabeled
-          label={item.label}
-          setter={setFormData}
-          name={name}
-          value={formData[name]}
+        <FormControlLabel
+          control={
+            <Checkbox
+              name={name}
+              checked={formData[name]}
+              onChange={handleChange}
+            />
+          }
+          label={<Typography variant="body2">{item.label}</Typography>}
         />
       </Box>
       {item?.value?.map((subItem: any, i: number) => (
