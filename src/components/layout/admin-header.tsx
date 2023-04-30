6fe6ab9bfc88db6c0082714452aaa9ea3c88d2a6
alirezakasirzare <alirezakasir@yahoo.com>
2023-04-30 10:23:08 +0330

    add confrim proccess modal and set sign out with it

--- src/components/layout/admin-header.tsx
+++ src/components/layout/admin-header.tsx
@@ -7,22 +7,28 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import LogoutIcon from "@mui/icons-material/Logout";
 import useLayoutStore from "hooks/store/layout-store";
 import PasswordIcon from "@mui/icons-material/Password";
+import FixedModal from "components/ui/modal/fixed-modal";
+import ChnagePasswordForm from "pages/auth/chnage-password-form";
 
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import FixedModal from "components/ui/modal/fixed-modal";
-import ChnagePasswordForm from "pages/auth/chnage-password-form";
+import ConfrimProccesModal from "components/ui/modal/confrim-procces-modal";
 
 function AdminHeader() {
   const toggleNormlize = useLayoutStore((state) => state.toggleNormlize);
 
   // sign out
+  const [confrimSignoutModal, setConfrimSignoutModal] = useState(false);
   const navigate = useNavigate();
   const handleSignOut = () => {
     localStorage.removeItem("token-auth");
     navigate("/");
   };
 
+  const handleConfrimSignOut = () => {
+    setConfrimSignoutModal(false);
+    handleSignOut();
+  };
   // change password
   const [isOpenChangePasswordModal, setIsOpenChangePasswordModal] =
     useState(false);
@@ -60,12 +66,23 @@ function AdminHeader() {
           <IconButton color="inherit" onClick={handleOpenChangePasswordModal}>
             <PasswordIcon />
           </IconButton>
-          <IconButton color="inherit" onClick={handleSignOut}>
+          <IconButton
+            color="inherit"
+            onClick={() => setConfrimSignoutModal(true)}
+          >
             <LogoutIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
 
+      {/* sign out modal */}
+      <ConfrimProccesModal
+        open={confrimSignoutModal}
+        onCancel={() => setConfrimSignoutModal(false)}
+        onConfrim={handleConfrimSignOut}
+        title="خروج از حساب کاربری"
+      />
+      {/* change password modal */}
       <FixedModal
         open={isOpenChangePasswordModal}
         handleClose={handleCloseChangePasswordModal}
---
+++ src/components/ui/modal/confrim-procces-modal.tsx
@@ -0,0 +1,78 @@
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import Typography from "@mui/material/Typography";
+
+import { createPortal } from "react-dom";
+import { ReactNode } from "react";
+import { styled } from "@mui/material/styles";
+import { Button, Stack } from "@mui/material";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: 0,
+  },
+
+  "& .MuiDialogTitle-root": {
+    fontSize: 14,
+  },
+
+  "& .MuiPaper-root": {},
+
+  "& .MuiBox-root": {
+    height: "100%",
+  },
+}));
+
+interface FixedModalProps {
+  open: boolean;
+  title?: string;
+  onCancel: () => void;
+  onConfrim: () => void;
+}
+
+function ConfrimProccesModal(props: FixedModalProps) {
+  const { open, onCancel, onConfrim, title } = props;
+
+  return createPortal(
+    <BootstrapDialog open={open} onClose={onCancel} maxWidth="sm" fullWidth>
+      <DialogTitle
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box>{title || "تایید کردن عملیات"}</Box>
+        <IconButton size="small" onClick={onCancel}>
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent dividers>
+        <Box maxHeight={1} padding={3}>
+          <Typography
+            variant="body1"
+            mb={2}
+            textAlign="center"
+            fontWeight={500}
+          >
+            آیا مایل به ادامه دادن هستید ؟
+          </Typography>
+
+          <Stack justifyContent="center" direction="row" spacing={1}>
+            <Button variant="contained" color="primary" onClick={onConfrim}>
+              تایید
+            </Button>
+            <Button variant="text" color="primary" onClick={onCancel}>
+              انصراف
+            </Button>
+          </Stack>
+        </Box>
+      </DialogContent>
+    </BootstrapDialog>,
+    document.body
+  );
+}
+
+export default ConfrimProccesModal;
