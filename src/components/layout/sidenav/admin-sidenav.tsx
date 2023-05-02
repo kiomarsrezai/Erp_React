1beafe04ca8a41e0167147d3f8233fb90a676d8c
alirezakasirzare <alirezakasir@yahoo.com>
2023-05-02 14:58:10 +0330

    add copy right in side menu

--- src/components/layout/sidenav/admin-sidenav.tsx
+++ src/components/layout/sidenav/admin-sidenav.tsx
@@ -4,6 +4,10 @@ import Divider from "@mui/material/Divider";
 import AdminSidenavMenu from "./admin-sidenav-menu";
 import AdminSidenavProfile from "./admin-sidenav-profile";
 import useLayoutStore from "hooks/store/layout-store";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CopyrightIcon from "@mui/icons-material/Copyright";
 
 function AdminSidenav() {
   const normalize = useLayoutStore((state) => state.normlize);
@@ -24,10 +28,33 @@ function AdminSidenav() {
       variant="permanent"
       anchor="left"
     >
-      <Box padding={1}>
-        <AdminSidenavProfile />
-        <Divider />
-        <AdminSidenavMenu />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        sx={{ height: "100%" }}
+      >
+        <Box padding={1}>
+          <AdminSidenavProfile />
+          <Divider />
+          <AdminSidenavMenu />
+        </Box>
+        <Card sx={{ textAlign: "center" }}>
+          <CardContent>
+            <Typography
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              gap={0.5}
+              variant="caption"
+              fontWeight="bold"
+              color="grey.600"
+            >
+              <CopyrightIcon sx={{ fontSize: 14 }} />
+              فاوا اهواز
+            </Typography>
+          </CardContent>
+        </Card>
       </Box>
     </Drawer>
   );
