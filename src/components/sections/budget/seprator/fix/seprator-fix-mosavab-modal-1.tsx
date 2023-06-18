6fc2fbf10db14788cd96e0a1b43fec1bfabc8766
Alireza Kasirzare <alirezakasir@yahoo.com>
2023-06-18 13:37:12 +0330

    add seprator mosavab edit inline

--- src/components/sections/budget/seprator/fix/seprator-fix-mosavab-modal-1.tsx
+++ src/components/sections/budget/seprator/fix/seprator-fix-mosavab-modal-1.tsx
@@ -1,5 +1,7 @@
 import FixedTable from "components/data/table/fixed-table";
 import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 import { TableHeadShape, TableHeadGroupShape } from "types/table-type";
 
 import {
@@ -10,10 +12,15 @@ import { sumFieldsInSingleItemData } from "helper/calculate-utils";
 import SectionGuard from "components/auth/section-guard";
 import { joinPermissions } from "helper/auth-utils";
 import { accessNamesConfig } from "config/access-names-config";
-import { Button, IconButton } from "@mui/material";
+import { Box, Button, IconButton, TextField } from "@mui/material";
 import { useState } from "react";
 import FixedModal from "components/ui/modal/fixed-modal";
 import SepratorFixMosavabModal2 from "./seprator-fix-mosavab-modal-2";
+import { useMutation } from "@tanstack/react-query";
+import { proposalBudgetApi } from "api/budget/proposal-api";
+import { globalConfig } from "config/global-config";
+import { enqueueSnackbar } from "notistack";
+import { sepratorBudgetApi } from "api/budget/seprator-api";
 
 interface SepratorFixMosavabModal1props {
   data: GetSingleSepratorMosavabItemShape[];
@@ -32,6 +39,7 @@ function SepratorFixMosavabModal1(props: SepratorFixMosavabModal1props) {
     {
       title: "کد",
       name: "code",
+      width: "140px",
     },
     {
       title: "شرح",
@@ -43,12 +51,47 @@ function SepratorFixMosavabModal1(props: SepratorFixMosavabModal1props) {
       align: "left",
       name: "mosavabPublic",
       split: true,
+      width: "160px",
     },
     {
       title: "عملیات",
       name: "actions",
+      width: "120px",
     },
   ];
+
+  // mosavab
+  const [editMosavab, setEditMosavab] = useState(0);
+
+  const renderMosavab = (item: GetSingleSepratorMosavabItemShape) => {
+    if (
+      item.budgetDetailId === activeMosavabItem?.budgetDetailId &&
+      item.budgetDetailProjectId === activeMosavabItem?.budgetDetailProjectId &&
+      item.budgetDetailProjectAreaId ===
+        activeMosavabItem?.budgetDetailProjectAreaId
+    ) {
+      return (
+        <TextField
+          id="mosavab-input"
+          label=""
+          variant="outlined"
+          type="number"
+          size="small"
+          value={editMosavab}
+          onChange={(e) => setEditMosavab(+e.target.value)}
+          autoComplete="off"
+          inputProps={{
+            sx: {
+              height: "17px",
+            },
+          }}
+          fullWidth
+        />
+      );
+    } else {
+      return item.mosavabProject;
+    }
+  };
 
   // actions
   const [isOpenMosavabFixModal, setIsOpenMosavabFixModal] = useState(false);
@@ -62,26 +105,77 @@ function SepratorFixMosavabModal1(props: SepratorFixMosavabModal1props) {
     // setDetailModalTitle(`${item.code} - ${item.description}`);
     // setCodingId(item[sepratorBudgetConfig.CODING]);
     setActiveMosavabItem(item);
-    setIsOpenMosavabFixModal(true);
+    setEditMosavab(item.mosavabPublic);
+    // setIsOpenMosavabFixModal(true);
+  };
+
+  const editMutation = useMutation(sepratorBudgetApi.fixMosavabUpdate, {
+    onSuccess: () => {
+      enqueueSnackbar(globalConfig.SUCCESS_MESSAGE, {
+        variant: "success",
+      });
+      // handleDoneActionTask();
+    },
+    onError: () => {
+      //enqueueSnackbar(globalConfig.ERROR_MESSAGE, {
+      //variant: "error",
+      //});
+    },
+  });
+
+  const onSubmitEditFunctionality = () => {
+    editMutation.mutate({
+      budgetDetailId: activeMosavabItem?.budgetDetailId,
+      budgetDetailProjectId: activeMosavabItem?.budgetDetailProjectId,
+      budgetDetailProjectAreaId: activeMosavabItem?.budgetDetailProjectAreaId,
+      mosavab: editMosavab,
+    });
   };
 
   const actionButtons = (item: GetSingleSepratorMosavabItemShape) => {
-    return (
-      <SectionGuard
-        permission={joinPermissions([
-          accessNamesConfig.BUDGET__SEPRATOR_PAGE,
-          accessNamesConfig.BUDGET__SEPRATOR_PAGE_FIX_MOSAVAB,
-        ])}
-      >
-        <IconButton
-          size="small"
-          color="primary"
-          onClick={() => handleClickFixMosavabModal(item)}
+    if (
+      item.budgetDetailId === activeMosavabItem?.budgetDetailId &&
+      item.budgetDetailProjectId === activeMosavabItem?.budgetDetailProjectId &&
+      item.budgetDetailProjectAreaId ===
+        activeMosavabItem?.budgetDetailProjectAreaId
+    ) {
+      return (
+        <Box display={"flex"} justifyContent={"center"}>
+          <IconButton
+            color="success"
+            size="small"
+            onClick={onSubmitEditFunctionality}
+          >
+            <CheckIcon />
+          </IconButton>
+
+          <IconButton
+            color="error"
+            size="small"
+            onClick={() => setActiveMosavabItem(null)}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      );
+    } else {
+      return (
+        <SectionGuard
+          permission={joinPermissions([
+            accessNamesConfig.BUDGET__SEPRATOR_PAGE,
+            accessNamesConfig.BUDGET__SEPRATOR_PAGE_FIX_MOSAVAB,
+          ])}
         >
-          <EditIcon />
-        </IconButton>
-      </SectionGuard>
-    );
+          <IconButton
+            size="small"
+            color="primary"
+            onClick={() => handleClickFixMosavabModal(item)}
+          >
+            <EditIcon />
+          </IconButton>
+        </SectionGuard>
+      );
+    }
   };
 
   // data
@@ -91,6 +185,7 @@ function SepratorFixMosavabModal1(props: SepratorFixMosavabModal1props) {
     const formatedData: any[] = unFormatData.map((item, i) => ({
       ...item,
       actions: () => actionButtons(item),
+      mosavabPublic: () => renderMosavab(item),
       number: i + 1,
     }));
 
