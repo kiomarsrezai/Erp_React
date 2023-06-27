ccd99cdae4a651ec0eb9eeba68527a052499ed43
Alireza Kasirzare <alirezakasir@yahoo.com>
2023-06-27 08:03:20 +0330

    fix coding

--- src/pages/budget/seprator-page.tsx
+++ src/pages/budget/seprator-page.tsx
@@ -364,7 +364,7 @@ document.querySelector('#table-container').scrollTo
 
   const tableData = sepratorQuery.data
     ? formatTableData(
-        sepratorQuery.data?.data.sort((a, b) => +a.code - +b.code)
+        sepratorQuery.data?.data //.sort((a, b) => +a.code - +b.code)
       )
     : [];
 
